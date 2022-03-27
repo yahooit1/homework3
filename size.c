dd47b0010d206f89f7be627322911c9dae808a9e
yahooit <rkgus6361@naver.com>
2022-03-28 02:26:17 +0900

    new version file

--- size.c
+++ size.c
@@ -4,7 +4,7 @@ void main()
 {
 printf("[----- [Gahyun] [2020045070] -----]\n");
 int **x;
-printf("sizeof(x) = %lu\n", sizeof(x)); //*x의 주소의 크기 8 bytes
-printf("sizeof(*x) = %lu\n", sizeof(*x)); //int형 변수 x의 주소의 크기 8 bytes
+printf("sizeof(x) = %lu\n", sizeof(x)); //*x의 주소의 크기
+printf("sizeof(*x) = %lu\n", sizeof(*x)); //int형 변수 x의 주소의 크기
 printf("sizeof(**x) = %lu\n", sizeof(**x));//int 형 변수 x의 실제값의 크기 4bytes
 }
